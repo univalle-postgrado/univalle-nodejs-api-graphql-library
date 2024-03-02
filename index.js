2de24cedc712dc801257443d99431e538dd47c89
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-03-02 16:06:23 -0400

    Definicion y Resolver de la Mutation para eliminar Book

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const typeDefs = `#graphql
       authorName: String
       authorNationality: String
     ): Book
+    deleteBook (id: String!): Book
   }
 
 `;
@@ -147,6 +148,14 @@ const resolvers = {
       };
       books[updatedBookIndex] = updatedBook;
       return updatedBook;
+    },
+    deleteBook: (root, {id}) => {
+      const deletedBookIndex = books.findIndex(book => book.id === id);
+
+      if (deletedBookIndex === -1) return null;
+
+      const deletedBook = books.splice(deletedBookIndex, 1)[0];
+      return deletedBook;
     }
   }
 };
